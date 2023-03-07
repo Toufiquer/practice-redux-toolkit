e9d5d7ae1d2f8cc5e4c409f4958e7f1675990588
Toufiquer <toufiquerabc@gmail.com>
2023-03-08 00:50:13 +0600

    design complete

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/video:id" element={<Video />} />
+        <Route path="/video/:id" element={<Video />} />
       </Routes>
     </>
   );
