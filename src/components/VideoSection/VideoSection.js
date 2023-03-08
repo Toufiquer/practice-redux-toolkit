ab6eb8d0b904fcbc62e23c07fd93de4baa70b9e5
Toufiquer <toufiquerabc@gmail.com>
2023-03-08 13:39:23 +0600

    design complete

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LikeUnLike from "./LikeUnLike";
 const VideoSection = () => {
   return (
     <>
@@ -29,32 +30,7 @@ const VideoSection = () => {
                   </h2>
 
                   {/* <!-- like/unlike --> */}
-                  <div className="flex gap-10 w-48">
-                    <div className="flex gap-1">
-                      <div className="shrink-0">
-                        <img
-                          className="w-5 block"
-                          src="./assets/like.svg"
-                          alt="Like"
-                        />
-                      </div>
-                      <div className="text-sm leading-[1.7142857] text-slate-600">
-                        100K
-                      </div>
-                    </div>
-                    <div className="flex gap-1">
-                      <div className="shrink-0">
-                        <img
-                          className="w-5 block"
-                          src="./assets/unlike.svg"
-                          alt="Unlike"
-                        />
-                      </div>
-                      <div className="text-sm leading-[1.7142857] text-slate-600">
-                        100K
-                      </div>
-                    </div>
-                  </div>
+                  <LikeUnLike />
                 </div>
 
                 <div className="mt-4 text-sm text-[#334155] dark:text-slate-400">
