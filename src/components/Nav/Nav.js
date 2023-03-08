80a1446eaf23d00e2194bb1327a9b084f3dc5faa
Toufiquer <toufiquerabc@gmail.com>
2023-03-09 02:10:13 +0600

    add search featured to store

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,15 @@ import React from "react";
 import logoImg from "../../components/assets/lws.svg";
 import searchImg from "../../components/assets/search.svg";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addSearch } from "../../redux/features/search/searchSlice";
 const Nav = () => {
+  const [text, setText] = React.useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addSearch(text));
+  };
   return (
     <>
       <nav className="bg-slate-100 shadow-md">
@@ -12,12 +20,14 @@ const Nav = () => {
           </Link>
           <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
             {/* <!-- search --> */}
-            <form>
+            <form onSubmit={(e) => handleSubmit(e)}>
               <input
                 className="outline-none border-none mr-2"
                 type="search"
                 name="search"
                 placeholder="Search"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
               />
             </form>
             <img
